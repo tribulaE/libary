7b3012554fb1c4079764854708cc4555eb4a0945
Nate <mpac4766@gmail.com>
2025-06-17 09:40:08 -0400

    Created a function to show each book on the page

--- java.js
+++ java.js
@@ -23,3 +23,26 @@ function Book(title, author, pages, read) {
         myLibrary.push(newBook)
         displayBooks();
     }
+
+    function displayBooks() {
+        const container = document.getElementById("book-container");
+        container.innerHTML = "";
+
+        myLibrary.forEach(book => {
+            const bookCard = document.createElement("div");
+            bookCard.classList.add("book-card")
+            bookCard.dataset.id = book.id;
+
+
+            bookCard.innerHTML = `
+            <h3>${book.title}</h3>
+            <p>by ${book.author}</p>
+            <p>${book.pages} pages</p>
+            <p>${book.read ? "Read" : "Not read yet"}</p>
+            <button class="remove-btn>Remove</button>
+            <button class="toggle-read-btn">Toggle Read</button
+            `;
+
+            container.appendChild(bookCard)
+        })
+        }
